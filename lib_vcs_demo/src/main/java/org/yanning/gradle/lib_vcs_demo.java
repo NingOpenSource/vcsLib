0b30203958730fdeccbc44fc754d0bd2c30ef378
Ning <765211630@qq.com>
2018-03-16 09:09:26 +0800

    删除无用的文件

--- lib_vcs_demo/src/main/java/org/yanning/gradle/lib_vcs_demo.java
+++
@@ -1,7 +0,0 @@
-package org.yanning.gradle;
-
-public class lib_vcs_demo {
-    public static void demo(){
-        System.err.println("----------------------------demo is running----------------------");
-    }
-}
