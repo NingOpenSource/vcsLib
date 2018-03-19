046d37b9eecd21ed4ce8ec782281c23be7ee29f2
Ning <765211630@qq.com>
2018-03-19 11:00:38 +0800

    正在加入git支持,1.0.23

--- README.md
+++ README.md
@@ -67,7 +67,7 @@ https://github.com/zacker330/jshint-gradle
     
     **IDE**：
     
-    ![](./pic/20180314162631.png)
+    ![](./pic/20180319105810.png)
         
 4. dependencies
         
--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,13 +1,15 @@
 import org.yanning.gradle.vcs_lib.extension.App
 import org.yanning.gradle.vcs_lib.extension.RepositoryGit
+import java.io.File
 
 fun main(args: Array<String>) {
-//    System.getProperties().setProperty(App.KEY_VCS_LIB_HOME,"D:\\yanning\\temp\\tt123")
-//    var repo= RepositoryGit()
-//    repo.username("admin")
-//    repo.password("admin")
+    System.getProperties().setProperty(App.KEY_VCS_LIB_HOME, File(System.getProperty("user.home"), ".vcsLib").absolutePath)
+    var repo= RepositoryGit()
+    repo.username("admin")
+    repo.password("admin")
 //    repo.url="http://admin@127.0.0.1:8080/r/temp123.git"
-//    repo.update()
-//    repo.commit()
-//    repo.upload()
+    repo.url="http://admin@127.0.0.1:8080/r/lib_arcface.git"
+    repo.update()
+    repo.commit()
+    repo.upload()
 }
