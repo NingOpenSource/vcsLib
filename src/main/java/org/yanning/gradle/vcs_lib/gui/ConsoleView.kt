0426a2d77dd18e923b6fc40984e6cc0687edc1db
Ning1994 <765211630@qq.com>
2019-03-07 18:00:39 +0800

    v2.0.0-alpha-002

--- src/main/java/org/yanning/gradle/vcs_lib/gui/ConsoleView.kt
+++ src/main/java/org/yanning/gradle/vcs_lib/gui/ConsoleView.kt
@@ -1,9 +1,64 @@
 package org.yanning.gradle.vcs_lib.gui
 
+import javafx.scene.control.Alert
 import tornadofx.*
 
-class ConsoleView : View("My View") {
-    override val root = borderpane {
+class ConsoleView : View("VCSLib-Console") {
+    override val root = vbox {
+        form {
+            fieldset("console") {
+                textarea {
+                }
+            }
+            fieldset("Repository") {
+                field("url") {
+                    textfield()
+                }
+                field("username") {
+                    textfield()
+                }
+                field("password") {
+                    textfield()
+                }
+                field("out path") {
+                    textfield()
+                    button("choose dir") {
+                        action {
+                            chooseDirectory {
+
+                            }
+                        }
+                    }
+                }
+                button("test connection") {
+                    action {
+                        alert(type = Alert.AlertType.INFORMATION, header = "Tip", content = "Connect success!")
+                    }
+                }
+            }
+            fieldset("Upload library") {
+                field("group id") {
+                    textfield()
+                }
+                field("artifact id") {
+                    textfield()
+                }
+                field("version") {
+                    textfield()
+                }
+                button("upload") {
+                    action {
+                        alert(type = Alert.AlertType.INFORMATION, header = "Tip", content = "Upload success!")
+                    }
+                }
+            }
+            button("update library") {
+                action {
+                    alert(type = Alert.AlertType.INFORMATION, header = "Tip", content = "Update success!")
+                }
+            }
+        }
 
     }
+
 }
